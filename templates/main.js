e5ed0f8ab67b0e6ef6797aac1b317ae45c83600a
vihdutta <vihdutta@gmail.com>
2022-10-05 03:21:00 -0400

    frontpage ui test 1

---
+++ templates/main.js
@@ -0,0 +1,17 @@
+/*
+var canvas = document.getElementById("canvas");
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
+
+console.log(canvas.width, canvas.height);
+
+if (canvas.getContext) {
+    layout = canvas.getContext("2d");
+    var img = new Image();
+    img.src = "./img.png";
+    img.onload = function() {
+        layout.drawImage(img, -100, -100, 500, 500);
+    };
+}
+
+*/
